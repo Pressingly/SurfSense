854392d979d5d527acd82d1409f976449fb6ab6a
CREDO23 <thierrybakera12@gmail.com>
2026-04-02 13:26:32 +0200

    feat: add macOS permission infrastructure for autocomplete

--- surfsense_desktop/src/ipc/handlers.ts
+++ surfsense_desktop/src/ipc/handlers.ts
@@ -1,19 +1,11 @@
 import { app, ipcMain, shell } from 'electron';
 import { IPC_CHANNELS } from './channels';
 import {
-  selectFolder,
-  addWatchedFolder,
-  removeWatchedFolder,
-  getWatchedFolders,
-  getWatcherStatus,
-  getPendingFileEvents,
-  acknowledgeFileEvents,
-  pauseWatcher,
-  resumeWatcher,
-  markRendererReady,
-  browseFiles,
-  readLocalFiles,
-} from '../modules/folder-watcher';
+  getPermissionsStatus,
+  requestAccessibility,
+  requestInputMonitoring,
+  restartApp,
+} from '../modules/permissions';
 
 export function registerIpcHandlers(): void {
   ipcMain.on(IPC_CHANNELS.OPEN_EXTERNAL, (_event, url: string) => {
@@ -31,40 +23,19 @@ export function registerIpcHandlers(): void {
     return app.getVersion();
   });
 
-  // Folder sync handlers
-  ipcMain.handle(IPC_CHANNELS.FOLDER_SYNC_SELECT_FOLDER, () => selectFolder());
-
-  ipcMain.handle(IPC_CHANNELS.FOLDER_SYNC_ADD_FOLDER, (_event, config) =>
-    addWatchedFolder(config)
-  );
-
-  ipcMain.handle(IPC_CHANNELS.FOLDER_SYNC_REMOVE_FOLDER, (_event, folderPath: string) =>
-    removeWatchedFolder(folderPath)
-  );
-
-  ipcMain.handle(IPC_CHANNELS.FOLDER_SYNC_GET_FOLDERS, () => getWatchedFolders());
-
-  ipcMain.handle(IPC_CHANNELS.FOLDER_SYNC_GET_STATUS, () => getWatcherStatus());
-
-  ipcMain.handle(IPC_CHANNELS.FOLDER_SYNC_PAUSE, () => pauseWatcher());
-
-  ipcMain.handle(IPC_CHANNELS.FOLDER_SYNC_RESUME, () => resumeWatcher());
-
-  ipcMain.handle(IPC_CHANNELS.FOLDER_SYNC_RENDERER_READY, () => {
-    markRendererReady();
+  ipcMain.handle(IPC_CHANNELS.GET_PERMISSIONS_STATUS, () => {
+    return getPermissionsStatus();
   });
 
-  ipcMain.handle(IPC_CHANNELS.FOLDER_SYNC_GET_PENDING_EVENTS, () =>
-    getPendingFileEvents()
-  );
+  ipcMain.handle(IPC_CHANNELS.REQUEST_ACCESSIBILITY, () => {
+    requestAccessibility();
+  });
 
-  ipcMain.handle(IPC_CHANNELS.FOLDER_SYNC_ACK_EVENTS, (_event, eventIds: string[]) =>
-    acknowledgeFileEvents(eventIds)
-  );
+  ipcMain.handle(IPC_CHANNELS.REQUEST_INPUT_MONITORING, () => {
+    requestInputMonitoring();
+  });
 
-  ipcMain.handle(IPC_CHANNELS.BROWSE_FILES, () => browseFiles());
-
-  ipcMain.handle(IPC_CHANNELS.READ_LOCAL_FILES, (_event, paths: string[]) =>
-    readLocalFiles(paths)
-  );
+  ipcMain.handle(IPC_CHANNELS.RESTART_APP, () => {
+    restartApp();
+  });
 }
